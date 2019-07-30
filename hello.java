f1bc2f0256fc8ca24f80090530251402a357b552
Sameer <sameerhankare@gmail.com>
2019-07-30 11:54:36 +0530

    Initial Commmit

--- hello.java
+++ hello.java
@@ -1,6 +1,20 @@
 public class hello{
+	static float add(float a,float b){
+		return a+b;
+	}
+	static float subtract(float a,float b){
+		return a-b;
+	}
+	static float multiply(float a,float b){
+		return a*b;
+	}
+	static float divide(float a,float b){
+			return a/b;
+	}
 	public static void main(String args[])
 	{
-		System.out.println("hello");
+		int a=5,b=6;
+
+		System.out.println("hello a="+a+" b="+b+" add="+add(a,b)+" subtract="+subtract(a,b)+" multiply="+multiply(a,b)+" divide="+divide(a,b));
 	}
 }
