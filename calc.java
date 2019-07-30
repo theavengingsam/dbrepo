4e27b8fd1f9fce5f34285464e7124a1b78223af1
Sameer <sameerhankare@gmail.com>
2019-07-30 12:19:56 +0530

    addded calc.java with qwertyuiop

--- calc.java
+++ calc.java
@@ -14,7 +14,7 @@ public class calc{
 	public static void main(String args[])
 	{
 		int a=5,b=6;
-
+		System.out.println("qwertyuiop");
 		System.out.println("hello a="+a+" b="+b+" add="+add(a,b)+" subtract="+subtract(a,b)+" multiply="+multiply(a,b)+" divide="+divide(a,b));
 	}
 }
